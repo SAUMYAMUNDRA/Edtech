1edec025aa2e48bf01e94f0b37db3059eb9c9ae1
Saumya Mundra <140224773+SAUMYAMUNDRA@users.noreply.github.com>
2025-09-08 08:45:53 +0530

    Update main.bat

--- main.bat
+++ main.bat
@@ -12,12 +12,6 @@ REM Start server (in a new window)
 cd /d "%ROOT_DIR%\server"
 start cmd /k "node index.js"
 
-REM Start XAMPP services (MySQL and Apache)
-cd /d "C:\xampp"
-start "" xampp_start.exe
-
-xampp-control.exe --startapache
-xampp-control.exe --startmysql
 
 ENDLOCAL
 pause
