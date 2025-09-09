61081f53c81c2ae13a56836fb905b606d91e6860
1st_learning <mundrasaumya17@gmail.com>
2025-09-09 15:09:26 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+import express from 'express';
 
+const app = express();
+const PORT = 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
