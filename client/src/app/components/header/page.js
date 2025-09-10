f680eb559373a2e3e641a9ee8fdf16ab6c0a64f3
1st_learning <mundrasaumya17@gmail.com>
2025-09-10 10:26:03 +0530

    added animations in home page

--- client/src/app/components/header/page.js
+++ client/src/app/components/header/page.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="w-full flex justify-between items-center px-10 py-5 shadow-xl bg-white font-bold text-xl">
+    <header className="w-full flex justify-between items-center px-10 py-5 shadow bg-white font-bold text-xl">
       {/* Logo */}
       <div className="flex items-center gap-2">
         <div className="w-8 h-8 bg-gray-900 flex justify-center items-center rounded">
@@ -14,10 +14,10 @@ export default function Header() {
 
       {/* Nav Links */}
       <nav className="flex gap-6 text-gray-700">
-        {["about", "testimonials", "mentorship", "university"].map((item, idx) => (
+        {["About", "Testimonials", "Mentorship", "For Universities"].map((item, idx) => (
           <a
             key={idx}
-             href={item === "mentorship" ? "/mentorship" : "#"}
+             href={item === "Mentorship" ? "/mentorship" : item==="About" ? "/home": "#"}
             className="relative px-3 py-1 rounded-md hover:text-black hover:bg-yellow-400 transition-colors duration-200
               after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300 hover:after:w-full"
           >
@@ -36,7 +36,7 @@ export default function Header() {
           Contact
         </a>
         <a
-          href="#"
+          href="/home"
           className="bg-yellow-400 text-black px-5 py-2.5 rounded-md font-medium shadow-xl"
         >
           Get Started
