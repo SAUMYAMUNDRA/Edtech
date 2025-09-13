f48e7efffca4d484e2c8a6ec8e978c32c787507f
1st_learning <mundrasaumya17@gmail.com>
2025-09-13 09:28:16 +0530

    added /university in header component

--- client/src/app/components/header/page.js
+++ client/src/app/components/header/page.js
@@ -17,7 +17,7 @@ export default function Header() {
         {["Home", "Testimonials", "Mentorship", "For Universities","About"].map((item, idx) => (
           <a
             key={idx}
-             href={item === "Mentorship" ? "/mentorship" : item==="Home" ? "/": item==="Testimonials" ? "/testimonial":item==="About" ? "/about": '#'}
+             href={item === "Mentorship" ? "/mentorship" : item==="Home" ? "/": item==="Testimonials" ? "/testimonial":item==="About" ? "/about": item=="For Universities"?"/university":"#"}
             className="hover:shadow-xl relative px-3 py-1 rounded-md hover:text-black hover:bg-yellow-400 transition-colors duration-200
               after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300 hover:after:w-full"
           >
