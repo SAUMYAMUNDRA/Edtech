6ccf746b1a195368aa5ccd5b284df7d78491ee37
Yukta233 <yukta0110@gmail.com>
2025-09-17 10:16:08 +0530

    Making another file for mentors

--- client/src/app/mentorship/mentors.js
+++ client/src/app/mentorship/mentors.js
@@ -1,181 +1,181 @@
-export const mentors = [
-  {
-    name: "Arish Rehman Khan",
-    role: "Software Engineer Systems, Ciena",
-    tags: ["Embedded Systems", "Project Management","Computer Networking"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/arishrehmankhan/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    description: "",
-    expertise: [
-      "Representational State Transfer(REST)",
-      "Android Development",
-      "Competitive coding guidance"
-    ],
-    whatYouGet: [
-      "1:1 Interview prep",
-      "Project-based feedback",
-      "Doubt solving and live sessions"
-    ]
-  },
-  {
-    name: "Dheeraj Yadav",
-    role: "Software Engineer, Cohesity",
-    tags: ["Golang", "Django","MongoDB"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/dheeraj-yadav-8b6917276/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    description: "",
-    expertise: [
-      "Swagger API",
-      "Ginkgo",
-      "Competitive Programming"
-    ],
-    whatYouGet: [
-      "Career mentorship",
-      "backend walkthroughs",
-      "Resume & code reviews"
-    ]
-  },
-  {
-    name: "Ruchira Naskar",
-    role: "SWE, Microsoft",
-    tags: ["Unit Testing", "Generative AI","Protocol Buffers"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/ruchira-naskar-18b86522a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    description: "",
-    expertise: [
-      "Dependency Injection",
-      "Remote Procedure Call(RPC)",
-      "Competitive Programming"
-    ],
-    whatYouGet: [
-      "Portfolio-ready projects",
-      "1:1 sessions",
-      "Feedback and problem-solving"
-    ]
-  },
-  {
-    name: "Sachin Gupta",
-    role: "Software Engineer, Uber",
-    tags: ["Software Development", "Go","OOP"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/sachin-k-gupta/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    description: "",
-    expertise: [
-      "Competitive Programming",
-      "TypeScript",
-      "Grafana"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Ekta Dhusia",
-    role: "Software Architect, Amdocs",
-    tags: ["Data Architecture", "Data Engineering","AWS"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/ekta-dhusia/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-    description: "",
-    expertise: [
-      "PySpark",
-      "Apache Pig",
-      "Hadoop"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Manmeet Chauhan",
-    role: "AVP Software Development, SitusAMC",
-    tags: ["Data Mapping", "Solution Architecture","Model-View-Controller(MVC)"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/manmeetchauhan/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-    description: "",
-    expertise: [
-      "Agile and Waterfall Methodologies",
-      "XSLT",
-      "AngularJS"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Kartar R.",
-    role: "Database Services Manager, Rackspace Technology",
-    tags: ["Microsoft SQL Server", "Database Administration","SQL"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/kartar-r-89b50546/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-    description: "",
-    expertise: [
-      "Microsoft Technologies",
-      "SQL Server Management Studio",
-      "Oracle Enterprise Manager"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Lov Vashist",
-    role: "Senior Sales Engineer, ColorTokens",
-    tags: ["Sales Engineer", "Keynote Delivery","Network Security"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/lov-vashist-8901811b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-    description: "",
-    expertise: [
-      "Technical Writing",
-      "Public Speaking",
-      "Cyber Resilience"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Harmanjot Singh",
-    role: "Software Engineer,Uber",
-    tags: ["SpringBoot", "Java","JUnit"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/harmanjot-singh-53aa38229/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    description: "",
-    expertise: [
-      "Software Engineering",
-      "Competetive Programming"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-  {
-    name: "Mohammad Inamul Haq",
-    role: "Senior Data Analyst, GrowthJockey",
-    tags: ["Business Intelligence", "Analytical Solutions","Web Analytics"],
-    photo: "",
-    linkedin: "https://www.linkedin.com/in/mohammad-inamul-haq-741901192/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-    description: "",
-    expertise: [
-      "Decision Making",
-      "Soft Skills",
-      "Business Analysis"
-    ],
-    whatYouGet: [
-      "End-to-end project help",
-      "Analytics labs",
-      "Interview and career guidance"
-    ]
-  },
-];
+ export const mentors = [
+    {
+      name: "Arish Rehman Khan",
+      role: "Software Engineer Systems, Ciena",
+      tags: ["Embedded Systems", "Project Management","Computer Networking"],
+      photo: "https://res.cloudinary.com/dsfssl5zk/image/upload/v1758082537/eoya4vvog1ilshljsyan.jpg",
+      linkedin: "https://www.linkedin.com/in/arishrehmankhan/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      description: "",
+      expertise: [
+        "Representational State Transfer(REST)",
+        "Android Development",
+        "Competitive coding guidance"
+      ],
+      whatYouGet: [
+        "1:1 Interview prep",
+        "Project-based feedback",
+        "Doubt solving and live sessions"
+      ]
+    },
+    {
+      name: "Dheeraj Yadav",
+      role: "Software Engineer, Cohesity",
+      tags: ["Golang", "Django","MongoDB"],
+      photo: "https://media.licdn.com/dms/image/v2/D4D03AQHvg1N8dbPPUg/profile-displayphoto-shrink_400_400/B4DZdA9ODIHkAk-/0/1749141482198?e=1761177600&v=beta&t=QGcuuPxzu6K88uGzBnnf9d6B4xrUuhfORuUH5Rh7D84",
+      linkedin: "https://www.linkedin.com/in/dheeraj-yadav-8b6917276/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      description: "",
+      expertise: [
+        "Swagger API",
+        "Ginkgo",
+        "Competitive Programming"
+      ],
+      whatYouGet: [
+        "Career mentorship",
+        "backend walkthroughs",
+        "Resume & code reviews"
+      ]
+    },
+    {
+      name: "Ruchira Naskar",
+      role: "SWE, Microsoft",
+      tags: ["Unit Testing", "Generative AI","Protocol Buffers"],
+      photo: "https://media.licdn.com/dms/image/v2/D5603AQHvgU3u33Li5g/profile-displayphoto-scale_400_400/B56Zkwqrw9I8Ag-/0/1757458121313?e=1761177600&v=beta&t=MFnEg7FPalyA415D1wRD1HOBOTiAZZTgs3irvz_V7B4",
+      linkedin: "https://www.linkedin.com/in/ruchira-naskar-18b86522a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      description: "",
+      expertise: [
+        "Dependency Injection",
+        "Remote Procedure Call(RPC)",
+        "Competitive Programming"
+      ],
+      whatYouGet: [
+        "Portfolio-ready projects",
+        "1:1 sessions",
+        "Feedback and problem-solving"
+      ]
+    },
+    {
+      name: "Sachin Gupta",
+      role: "Software Engineer, Uber",
+      tags: ["Software Development", "Go","OOP"],
+      photo: "https://media.licdn.com/dms/image/v2/D5603AQHNdG66BHt4GA/profile-displayphoto-scale_400_400/B56Zh2GhmcHMAk-/0/1754328082991?e=1761177600&v=beta&t=8O-BmqHKw201RNS3lz9HC81eNEqg7IlGophEDL74sF4",
+      linkedin: "https://www.linkedin.com/in/sachin-k-gupta/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      description: "",
+      expertise: [
+        "Competitive Programming",
+        "TypeScript",
+        "Grafana"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+    {
+      name: "Ekta Dhusia",
+      role: "Software Architect, Amdocs",
+      tags: ["Data Architecture", "Data Engineering","AWS"],
+      photo: "https://media.licdn.com/dms/image/v2/C4E03AQE-LEU3g5F9tg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1609785350202?e=1761177600&v=beta&t=0V1ImoF4S4yGoEIhKXxx-lt66bYIBhOh0THFLjJQ1z8",
+      linkedin: "https://www.linkedin.com/in/ekta-dhusia/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      description: "",
+      expertise: [
+        "PySpark",
+        "Apache Pig",
+        "Hadoop"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+    {
+      name: "Manmeet Chauhan",
+      role: "AVP Software Development, SitusAMC",
+      tags: ["Data Mapping", "Solution Architecture","Model-View-Controller(MVC)"],
+      photo: "https://media.licdn.com/dms/image/v2/C4D03AQGBTSRZb_1BAA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1516626173865?e=1761177600&v=beta&t=t_cjmDmUXvbtQAfzSNPvhNO0GrAWX5oBMIYbYMxUFwA",
+      linkedin: "https://www.linkedin.com/in/manmeetchauhan/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      description: "",
+      expertise: [
+        "Agile and Waterfall Methodologies",
+        "XSLT",
+        "AngularJS"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+    {
+      name: "Kartar R.",
+      role: "Database Services Manager, Rackspace Technology",
+      tags: ["Microsoft SQL Server", "Database Administration","SQL"],
+      photo: "https://media.licdn.com/dms/image/v2/C5603AQEj6CmSlJ754Q/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1517431664746?e=1761177600&v=beta&t=xGVb5p0hTAcOmhYblTc9kcu6CsTHxqK2l6dri4XaBlE",
+      linkedin: "https://www.linkedin.com/in/kartar-r-89b50546/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      description: "",
+      expertise: [
+        "Microsoft Technologies",
+        "SQL Server Management Studio",
+        "Oracle Enterprise Manager"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+    {
+      name: "Lov Vashist",
+      role: "Senior Sales Engineer, ColorTokens",
+      tags: ["Sales Engineer", "Keynote Delivery","Network Security"],
+      photo: "https://media.licdn.com/dms/image/v2/D5603AQEAKt7f6woxAw/profile-displayphoto-scale_400_400/B56ZhQlgThHUAo-/0/1753698667456?e=1761177600&v=beta&t=p2EQxEk-RVC1NqHwHdf9PuYfOTDpipI5kP8p-aoJtuo",
+      linkedin: "https://www.linkedin.com/in/lov-vashist-8901811b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      description: "",
+      expertise: [
+        "Technical Writing",
+        "Public Speaking",
+        "Cyber Resilience"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+     {
+      name: "Harmanjot Singh",
+      role: "Software Engineer,Uber",
+      tags: ["SpringBoot", "Java","JUnit"],
+      photo: "https://media.licdn.com/dms/image/v2/D5603AQFHBbV5hzydLg/profile-displayphoto-scale_400_400/B56ZjCcE_5HcAk-/0/1755608804267?e=1761177600&v=beta&t=d6FsibMZKHE8zgcqChWXEDjMR1hRLZyC5sOIk8_GRcY",
+      linkedin: "https://www.linkedin.com/in/harmanjot-singh-53aa38229/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      description: "",
+      expertise: [
+        "Software Engineering",
+        "Competetive Programming"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+     {
+      name: "Mohammad Inamul Haq",
+      role: "Senior Data Analyst, GrowthJockey",
+      tags: ["Business Intelligence", "Analytical Solutions","Web Analytics"],
+      photo: "https://media.licdn.com/dms/image/v2/D5603AQHBSAtxyEi3yw/profile-displayphoto-shrink_400_400/B56ZgnBkweG4As-/0/1753001382636?e=1761177600&v=beta&t=MCU85en6hdAXwaLuWCkCh0eBRQ7ZbOuzo5GD-pcgneY",
+      linkedin: "https://www.linkedin.com/in/mohammad-inamul-haq-741901192/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      description: "",
+      expertise: [
+        "Decision Making",
+        "Soft Skills",
+        "Business Analysis"
+      ],
+      whatYouGet: [
+        "End-to-end project help",
+        "Analytics labs",
+        "Interview and career guidance"
+      ]
+    },
+  ];
